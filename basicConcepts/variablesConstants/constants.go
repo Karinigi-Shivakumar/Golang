8710f3d24b9ab1f3f882ed7683ed94916ebd17af
Karinigi-Shivakumar <shivakumarkarinigi@gmail.com>
2024-03-02 15:38:41 +0530

    feat:variables and constants

---
+++ basicConcepts/variablesConstants/constants.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func constants() {
+	//constants are used declare constants if any, they can be declared anywhere where variables can be declared
+	const n = 9
+	const a = "chara"
+	fmt.Print(n, a)
+}
---
+++ basicConcepts/variablesConstants/variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func variables() {
+	var a int = 1
+	var c, b string = "ab", "cd"
+	var variable1 = "shiva"
+	var d int
+	e := "trans" // this syntax can only be used inside functions
+	fmt.Println(a, b, c, d, e, variable1)
+}
